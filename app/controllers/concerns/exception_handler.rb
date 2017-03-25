8064820da6a0995d8d75f02466bd2a5e2dbdb0e7
Alcasser <alcacer.la.1001@gmail.com>
2017-03-26 00:41:12 +0100

    issue controller unfinished

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,14 @@
+# Handles controller exceptions
+module ExceptionHandler
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      render json: { message: e.message }, status: :not_found
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      render json: { message: e.message }, status: :bad_request
+    end
+  end
+end
