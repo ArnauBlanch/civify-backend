6e11b3b5abc96ac58b96a5ea2e3644f1d3de215f
Alcasser <alcacer.la.1001@gmail.com>
2017-03-25 02:03:00 +0100

    model Issue + tests (no code style)

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170325002420) do
 
+  create_table "issues", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.float    "latitude",    limit: 24
+    t.float    "longitude",   limit: 24
+    t.string   "category"
+    t.string   "picture"
+    t.string   "description"
+    t.boolean  "risk"
+    t.boolean  "solved"
+    t.integer  "reports"
+    t.integer  "supports"
+    t.integer  "user_id"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["user_id"], name: "index_issues_on_user_id", using: :btree
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
+  end
+
+  add_foreign_key "issues", "users"
 end
