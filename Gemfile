46401a57c5200ab911446f527b756f426b9473c6
Alcasser <alcacer.la.1001@gmail.com>
2017-03-26 19:15:26 +0200

    issue controller finished (no tests)

--- Gemfile
+++ Gemfile
@@ -12,7 +12,9 @@ gem 'mysql2', '>= 0.3.18', '< 0.5'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 
-gem 'factory_girl_rails', '~> 4.0'
+# Gem for image uploading (install ImageMagick tool first)
+gem 'paperclip'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326015701) do
+ActiveRecord::Schema.define(version: 20170326103244) do
 
   create_table "issues", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
-    t.float    "latitude",         limit: 24
-    t.float    "longitude",        limit: 24
+    t.float    "latitude",             limit: 24
+    t.float    "longitude",            limit: 24
     t.string   "category"
-    t.string   "picture"
     t.string   "description"
     t.boolean  "risk"
     t.integer  "reports"
     t.integer  "confirm_votes"
     t.integer  "user_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.integer  "resolved_votes"
     t.string   "issue_auth_token"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
     t.index ["issue_auth_token"], name: "index_issues_on_issue_auth_token", unique: true, using: :btree
     t.index ["user_id"], name: "index_issues_on_user_id", using: :btree
   end
