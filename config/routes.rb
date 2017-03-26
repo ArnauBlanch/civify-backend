69dc7af1e4bafdc9801d7d7748edd14edbbdea41
Alcasser <alcacer.la.1001@gmail.com>
2017-03-26 10:34:40 +0200

    issue controller with token no picture

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users do resources :issues end
+  resources :users, param: :user_auth_token do
+    member do
+      resources :issues, param: :issue_auth_token
+    end
+  end
 end
