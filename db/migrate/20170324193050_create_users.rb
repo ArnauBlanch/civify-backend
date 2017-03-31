b26c9f982ca4d02fd1d6cca79c863d586c69d8bb
Alcasser <alcacer.la.1001@gmail.com>
2017-03-31 17:06:02 +0200

    small integration fix

--- db/migrate/20170324193050_create_users.rb
+++
@@ -1,8 +0,0 @@
-# Migration file of the users table creation free comment please give me coins
-class CreateUsers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :users do |t|
-      t.timestamps
-    end
-  end
-end
