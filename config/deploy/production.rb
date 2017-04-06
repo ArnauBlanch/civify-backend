b35f162c1d6e7f9d5eec31ae5dd8cf8e7c2c2c11
Arnau Blanch Cortès <arnaubc@gmail.com>
2017-04-07 01:51:28 +0200

    Capistrano Passenger setup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@
 # You can define all roles on a single server, or split them:
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
+server "civify.cf", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
 
@@ -17,9 +17,9 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+role :app, %w{deploy@civify.cf}
+role :web, %w{deploy@civify.cf}
+role :db,  %w{deploy@civify.cf}
 
 
 
