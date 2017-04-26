7e61b654cdec72d9c4bf3e8adc7682eec0e8fe42
Alcasser <alcacer.la.1001@gmail.com>
2017-04-26 20:06:44 +0200

    paperclip large_url now is original size and votes fix

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -18,6 +18,7 @@ class IssuesController < ApplicationController
     set_user
     @issue = @user.issues.build(issue_params)
     @issue.picture = @picture
+    @issue.resolved_votes = 0
     @issue.save!
     @issue.current_user = current_user
     json_response(@issue, :created)
