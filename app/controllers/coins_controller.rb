03d959dc88c35b2a03b237fab2d9de9c8215c80c
Iván de Mingo Guerrero <ivandemingo2309@gmail.com>
2017-05-04 20:40:37 +0200

    Create coins route + implement request to add coins

---
+++ app/controllers/coins_controller.rb
@@ -0,0 +1,24 @@
+class CoinsController < ApplicationController
+  before_action :set_user
+
+  skip_before_action :authenticate_request
+
+  # POST /users/:user_auth_token/coins
+  def create
+    if params[:coins].nil?
+      render json: { message: 'Specify the number of coins' }, status:
+          :bad_request
+    else
+      @user.coins += params[:coins]
+      @user.save(validate: false)
+      render json: @user, status: :ok
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(user_auth_token: params[:user_auth_token])
+    render json: { message: 'User not found' }, status: :not_found if @user.nil?
+  end
+end
---
+++ test/controllers/coins_controller_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class CoinsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    setup_user
+  end
+
+  test 'user not found' do
+    post "/users/1234/coins",
+         headers: authorization_header(@password, @user.username),
+         params: { coins: 5 }, as: :json
+    assert_response :not_found
+    body = JSON.parse(response.body)
+    assert_equal 'User not found', body['message']
+  end
+
+  test 'coins not specified' do
+    post "/users/#{@user.user_auth_token}/coins",
+         headers: authorization_header(@password, @user.username)
+    assert_response :bad_request
+    body = JSON.parse(response.body)
+    assert_equal 'Specify the number of coins', body['message']
+  end
+
+  test 'valid add coins' do
+    before_coins = @user.coins
+    post "/users/#{@user.user_auth_token}/coins",
+         headers: authorization_header(@password, @user.username),
+         params: { coins: 5 }, as: :json
+    assert_response :ok
+    body = JSON.parse(response.body)
+    assert_equal before_coins + 5, body['coins']
+    assert_equal before_coins + 5, User.find(@user.id).coins
+  end
+end
