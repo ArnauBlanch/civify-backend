cdcc8d0a7c753fb6850eb9bd3a7fdc8bf0699ec0
Carleslc <lazaro.costa.carles@gmail.com>
2017-05-06 16:35:30 +0200

    Creation rewards test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  include RewardsConstants
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
   # order.
   fixtures :all
