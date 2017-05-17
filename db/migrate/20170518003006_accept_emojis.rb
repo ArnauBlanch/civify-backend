d705d8493bbb309f331206e2d322b90a17a3e6fe
Carleslc <lazaro.costa.carles@gmail.com>
2017-05-18 01:11:59 +0200

    UTF8MB4 collation unicode

--- db/migrate/20170518003006_accept_emojis.rb
+++ db/migrate/20170518003006_accept_emojis.rb
@@ -1,9 +1,9 @@
 class AcceptEmojis < ActiveRecord::Migration[5.0]
   def change
     # for each table that will store unicode (like emojis) execute:
-    execute "ALTER TABLE issues CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
-    execute "ALTER TABLE issues CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
-            MODIFY title TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
-            MODIFY description TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    execute "ALTER TABLE issues CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
+    execute "ALTER TABLE issues CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+            MODIFY title TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+            MODIFY description TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
   end
 end
