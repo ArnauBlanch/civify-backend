2874c2728353c314aac01fd8237e05eaa80c139d
Carleslc <lazaro.costa.carles@gmail.com>
2017-05-18 22:51:19 +0200

    Authorization fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,97 +3,19 @@ class ApplicationController < ActionController::API
   include ExceptionHandler
   include Response
   include RewardsConstants
+  include AuthorizationController
+
   # Requires authentication token before requesting resources
-  # Moreover checks that update target is the current user
-  before_action :verify_user, :verify_issue, :verify_award, :authenticate_request
+  # Skip if route does not require Authorization header
+  before_action :authenticate_request
 
-  attr_reader :current_user
+  # Checks that update target is the current user unless is admin
+  # Skip if route can perform a non-GET method with user_auth_token different than current user
+  before_action :verify_user, :verify_issue, :verify_award
 
-  private
-
-  def authenticate_request
-    auth_command = AuthorizeApiRequest.call(request.headers)
-    if auth_command.success?
-      @current_user = auth_command.result
-      verify_award_auth? if verify_user_auth? and verify_issue_auth?
-    else
-      render json: { message: auth_command.errors.values[0].first }, status: :unauthorized
-    end
-  end
-
-  def verify_user_auth?
-    user_auth_token = params[:user_auth_token]
-    return true unless user_auth_token
-    return false unless check_user_exists(User.find_by_user_auth_token(user_auth_token))
-    return true unless @verify_user
-    if critical_request? && !current_user?(user_auth_token)
-      render json: { message: 'Cannot update other users' }, status: :unauthorized
-      return false
-    end
-    true
-  end
-
-  def verify_issue_auth?
-    issue_auth_token = params[:issue_auth_token]
-    return true unless issue_auth_token
-    issue = Issue.find_by_issue_auth_token(issue_auth_token)
-    return false unless check_issue_exists(issue)
-    return true unless @verify_issue
-    if critical_request? && !current_user?(issue.user.user_auth_token)
-      render json: { message: "Cannot update other's issues" }, status: :unauthorized
-      false
-    end
-  end
-
-  def verify_award_auth?
-    award_auth_token = params[:award_auth_token]
-    return true unless award_auth_token
-    award = Award.find_by_award_auth_token(award_auth_token)
-    return false unless check_award_exists(award)
-    return true unless @verify_award
-    if critical_request? && !current_user?(award.commerce_offering.user_auth_token)
-      render json: { message: "Cannot update other's awards" }, status: :unauthorized
-      false
-    end
-  end
-
-  def verify_user
-    @verify_user = true
-  end
-
-  def verify_issue
-    @verify_issue = true
-  end
-
-  def verify_award
-    @verify_award = true
-  end
-
-  def critical_request?
-    !@current_user.admin? && request.request_method != 'GET'
-  end
-
-  def current_user?(user_auth_token)
-    user_auth_token == @current_user.user_auth_token
-  end
-
-  def check_user_exists(user)
-    return true if user
-    render json: { message: 'User not found' }, status: :not_found
-    false
-  end
-
-  def check_issue_exists(issue)
-    return true if issue
-    render json: { message: 'Issue not found' }, status: :not_found
-    false
-  end
-
-  def check_award_exists(award)
-    return true if award
-    render json: { message: 'Award not found' }, status: :not_found
-    false
-  end
+  # Checks that params exists and performs security verifications if needed
+  # PLEASE, DO NOT SKIP THIS
+  before_action :verify_user_auth, :verify_issue_auth, :verify_award_auth
 
   def add_reward!(user, coins = 0, xp = 0)
     user.coins += coins
