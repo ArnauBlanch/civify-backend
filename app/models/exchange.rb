e8472bc4e05c8a2cfede12d9833e90553e95f06c
Iván de Mingo Guerrero <ivandemingo2309@gmail.com>
2017-05-19 18:35:29 +0200

    Check exchange code presence

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -2,4 +2,5 @@ class Exchange < ApplicationRecord
   belongs_to :award
   belongs_to :user
   validates_uniqueness_of :award_id, scope: :user_id
+  validates :exchange_auth_token, presence: true
 end
