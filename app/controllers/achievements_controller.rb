4b3e8f31a28d92ee0a449fdb64f3f8cd419efe6c
Iván de Mingo Guerrero <ivandemingo2309@gmail.com>
2017-05-31 21:14:47 +0200

    Fix create achievement response status

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,10 +1,12 @@
 class AchievementsController < ApplicationController
+  skip_before_action :authenticate_request
+
   def create
     @achievement = Achievement.new(achievement_params)
     if @achievement.save
       render json: @achievement, status: :created
     else
-      render json: { message: 'Achievement not created' }
+      render json: { message: 'Achievement not created' }, status: :bad_request
     end
   end
 
