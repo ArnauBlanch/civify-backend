0dd684e47cba47568579b45896e90ffb6ba28074
Alcasser <alcacer.la.1001@gmail.com>
2017-06-05 19:44:27 +0200

    give badge callbacks to event and achievement

--- app/models/event_progress.rb
+++ app/models/event_progress.rb
@@ -1,4 +1,5 @@
 class EventProgress < ApplicationRecord
+  after_save :give_badge if :completed_changed?
   belongs_to :user
   belongs_to :event
   validates_uniqueness_of :user_id, scope: :event_id
@@ -7,5 +8,11 @@ class EventProgress < ApplicationRecord
 
   def as_json(options = nil)
     event.as_json(options)
+  end
+
+  private
+
+  def give_badge
+    user.badges << event.badge if completed
   end
 end
