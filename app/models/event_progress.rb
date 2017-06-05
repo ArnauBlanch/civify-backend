b7e5271ad82007fc6c6f07daede96130e486b579
Alcasser <alcacer.la.1001@gmail.com>
2017-06-05 23:04:32 +0200

    modifications, doing the same different progress

--- app/models/event_progress.rb
+++ app/models/event_progress.rb
@@ -1,5 +1,4 @@
 class EventProgress < ApplicationRecord
-  after_save :give_badge if :completed_changed?
   belongs_to :user
   belongs_to :event
   validates_uniqueness_of :user_id, scope: :event_id
@@ -8,11 +7,5 @@ class EventProgress < ApplicationRecord
 
   def as_json(options = nil)
     event.as_json(options)
-  end
-
-  private
-
-  def give_badge
-    user.badges << event.badge if completed
   end
 end
