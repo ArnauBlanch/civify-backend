1064279b894d58cf1d66775fc8c7fbd1e8342140
Alcasser <alcacer.la.1001@gmail.com>
2017-06-05 23:04:32 +0200

    modifications, doing the same different progress

--- app/models/achievement_progress.rb
+++ app/models/achievement_progress.rb
@@ -1,5 +1,4 @@
 class AchievementProgress < ApplicationRecord
-  after_save :give_badge if :completed_changed?
   belongs_to :user
   belongs_to :achievement
   validates_uniqueness_of :user_id, scope: :achievement_id
@@ -8,11 +7,5 @@ class AchievementProgress < ApplicationRecord
 
   def as_json(options = nil)
     achievement.as_json(options)
-  end
-
-  private
-
-  def give_badge
-    user.badges << achievement.badge if completed
   end
 end
